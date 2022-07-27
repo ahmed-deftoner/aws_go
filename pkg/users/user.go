75775aede649b99eb534c8d200dabbfce51fed95
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-27 19:43:24 +0500

    error vars

--- pkg/users/user.go
+++ pkg/users/user.go
@@ -9,7 +9,17 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-var ()
+var (
+	ErrorFailedToUnmarshalRecord = "failed to unmarshal record"
+	ErrorFailedToFetchRecord     = "failed to fetch record"
+	ErrorInvalidUserData         = "invalid user data"
+	ErrorInvalidEmail            = "invalid email"
+	ErrorCouldNotMarshalItem     = "could not marshal item"
+	ErrorCouldNotDeleteItem      = "could not delete item"
+	ErrorCouldNotDynamoPutItem   = "could not dynamo put item"
+	ErrorUserAlreadyExists       = "user.User already exists"
+	ErrorUserDoesNotExist        = "user.User does not exist"
+)
 
 type User struct {
 	Email     string `json:"email"`
