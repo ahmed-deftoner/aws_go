8f5a4f8c394301545a297f3f888913f20aa1a0b2
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-27 19:36:48 +0500

    user struct

--- pkg/users/user.go
+++ pkg/users/user.go
@@ -2,6 +2,12 @@ package users
 
 var ()
 
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
 func FetchUser() {
 
 }
