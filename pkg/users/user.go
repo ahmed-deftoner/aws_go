761174d9a894a955bdc82b49733a563da8dc7693
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-27 19:43:00 +0500

    fetch user

--- pkg/users/user.go
+++ pkg/users/user.go
@@ -1,5 +1,14 @@
 package users
 
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
 var ()
 
 type User struct {
@@ -8,8 +17,27 @@ type User struct {
 	LastName  string `json:"lastName"`
 }
 
-func FetchUser() {
+func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*User, error) {
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
 
+	result, err := dynaClient.GetItem(input)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+	item := new(User)
+	err = dynamodbattribute.UnmarshalMap(result.Item, item)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+	return item, nil
 }
 
 func FetchUsers() {
