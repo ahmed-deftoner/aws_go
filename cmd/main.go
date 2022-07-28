eb7400d7f90043d7840c68a73024efdaa598dacd
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-28 20:18:43 +0500

    table name

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,7 @@ func main() {
 	lambda.Start(handler)
 }
 
-const tableName = "go-serverless-yt"
+const tableName = "Ahmed_Deftoner"
 
 func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	switch req.HTTPMethod {
